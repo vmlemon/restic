50e9654ddcf16614d31a6b3605eb16364b62bc63
Tyson Key <tyson.key@gmail.com>
2024-12-16 00:48:23 +0000

    Fix lint error

--- internal/fs/node_xattr_all_test.go
+++ internal/fs/node_xattr_all_test.go
@@ -1,4 +1,4 @@
-//go:build darwin || freebsd || netbsd ||  linux || solaris || windows
+//go:build darwin || freebsd || netbsd || linux || solaris || windows
 // +build darwin freebsd netbsd linux solaris windows
 
 package fs
